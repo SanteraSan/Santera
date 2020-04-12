bc65b0d5a76d29ca3d1e6e9b14087fdc9c5a6c4b
SanteraSan <santera102@gmail.com>
2020-04-12 19:44:28 +0500

    Update Content.jsx

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Profile from "./Profile/Profile";
 import style from "./Style.module.css"
-import Dialogs from "./Dialogs/Dialogs";
+import Dialogs from "./Dialogs/Dialogs"
 
 const Content = () =>
     <div className={style.content}>
